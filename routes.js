6a53749592328b2ebcfd356354d3abb23228d5e8
kwlodarska <katarzyna.wlodarska@schibsted.pl>
2016-03-17 10:36:46 +0100

    next

--- routes.js
+++ routes.js
@@ -1,14 +1,14 @@
 module.exports = function(stockRepository) {
     return {
         findAll: function (req, res, next) {
-            stockRepository().findAll().then(function (docs) {
+            stockRepository.findAll().then(function (docs) {
                 res.json(docs);
             }).catch(next);
         },
         getCounter: function (req, res, next) {
             var isbn = req.params.isbn;
 
-            stockRepository().getCount(isbn).then(function (result) {
+            stockRepository.getCount(isbn).then(function (result) {
                 if (result !== null)
                     res.status(200).json({count: result});
                 else
@@ -19,7 +19,7 @@ module.exports = function(stockRepository) {
             var isbn = req.body.isbn;
             var count = req.body.count;
 
-            stockRepository().stockUp(isbn, count).then(function () {
+            stockRepository.stockUp(isbn, count).then(function () {
                 res.json({isbn: req.body.isbn, count: req.body.count});
             }).catch(next);
         }
